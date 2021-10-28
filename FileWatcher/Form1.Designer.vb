abb3e2824ac6fb290959a8e6594d28c80fbcd7ad
chu arai <iwasaki@daisosystem.co.jp>
2021-10-28 15:17:39 +0900

    first

--- FileWatcher/Form1.Designer.vb
+++ FileWatcher/Form1.Designer.vb
@@ -22,10 +22,71 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(21, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(580, 19)
+        Me.TextBox1.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(21, 67)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(580, 352)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(91, -3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(440, -3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(102, 29)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(628, 431)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- FileWatcher/Form1.vb
+++ FileWatcher/Form1.vb
@@ -1,3 +1,102 @@
 ﻿Public Class Form1
 
+
+
+
+
+
+
+
+
+    Private Sub TextBox1_DragEnter(sender As Object, e As DragEventArgs) Handles TextBox1.DragEnter
+        'ファイル形式の場合のみ、ドラッグを受け付けます。
+        If e.Data.GetDataPresent(DataFormats.FileDrop) = True Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub TextBox1_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox1.DragDrop
+        'ドラッグされたファイル・フォルダのパスを格納します。
+        Dim strFileName As String() = CType(e.Data.GetData(DataFormats.FileDrop, False), String())
+
+        'ファイルの存在確認を行い、ある場合にのみ、
+        'テキストボックスにパスを表示します。
+        '（この処理でフォルダを対象外にしています。）
+        If System.IO.File.Exists(strFileName(0).ToString) = True Then
+            Me.TextBox1.Text = strFileName(0).ToString
+        ElseIf System.IO.Directory.Exists(strFileName(0).ToString) = True Then
+            Me.TextBox1.Text = strFileName(0).ToString
+        End If
+    End Sub
+
+    Private watcher As System.IO.FileSystemWatcher = Nothing
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+
+
+        ListBox1.Items.Add("aaaaa")
+
+
+        If Not (watcher Is Nothing) Then
+            Return
+        End If
+
+        watcher = New System.IO.FileSystemWatcher
+        '監視するディレクトリを指定
+        watcher.Path = TextBox1.Text
+        '最終アクセス日時、最終更新日時、ファイル、フォルダ名の変更を監視する
+        watcher.NotifyFilter = System.IO.NotifyFilters.LastAccess Or
+            System.IO.NotifyFilters.LastWrite Or
+            System.IO.NotifyFilters.FileName Or
+            System.IO.NotifyFilters.DirectoryName
+        'すべてのファイルを監視
+        watcher.Filter = ""
+        watcher.IncludeSubdirectories = True
+        'UIのスレッドにマーシャリングする
+        'コンソールアプリケーションでの使用では必要ない
+        watcher.SynchronizingObject = Me
+
+        'イベントハンドラの追加
+        AddHandler watcher.Changed, AddressOf watcher_Changed
+        AddHandler watcher.Created, AddressOf watcher_Changed
+        AddHandler watcher.Deleted, AddressOf watcher_Changed
+        AddHandler watcher.Renamed, AddressOf watcher_Renamed
+
+        '監視を開始する
+        watcher.EnableRaisingEvents = True
+        ListBox1.Items.Add("監視を開始しました。")
+
+    End Sub
+
+    'イベントハンドラ
+    Private Sub watcher_Changed(ByVal source As System.Object,
+        ByVal e As System.IO.FileSystemEventArgs)
+        Select Case e.ChangeType
+            Case System.IO.WatcherChangeTypes.Changed
+                ListBox1.Items.Add(("ファイル 「" + e.FullPath +
+                    "」が変更されました。"))
+            Case System.IO.WatcherChangeTypes.Created
+                ListBox1.Items.Add(("ファイル 「" + e.FullPath +
+                    "」が作成されました。"))
+            Case System.IO.WatcherChangeTypes.Deleted
+                ListBox1.Items.Add(("ファイル 「" + e.FullPath +
+                    "」が削除されました。"))
+        End Select
+    End Sub
+
+    Private Sub watcher_Renamed(ByVal source As System.Object,
+        ByVal e As System.IO.RenamedEventArgs)
+        ListBox1.Items.Add(("ファイル 「" + e.FullPath +
+            "」の名前が変更されました。"))
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '監視を終了
+        watcher.EnableRaisingEvents = False
+        watcher.Dispose()
+        watcher = Nothing
+        ListBox1.Items.Add("監視を終了しました。")
+    End Sub
 End Class
